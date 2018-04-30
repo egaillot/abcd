baf735c82efdab5698f9efe7b704e602af5499c3
Emmanuel Gaillot <emmanuel.gaillot@gmail.com>
2018-04-30 18:48:40 +0200

    Compute week type for any week number

--- test/abcd.test.js
+++ test/abcd.test.js
@@ -7,4 +7,28 @@ describe("The ABCD-er", function () {
         weekType = abcder.fromWeek(1);
     expect(weekType).to.eql("A");
   });
+
+  it("tells second week of the year is type B", function () {
+    var abcder = ABCD.newAbcder(),
+        weekType = abcder.fromWeek(2);
+    expect(weekType).to.eql("B");
+  });
+
+  it("tells third week of the year is type C", function () {
+    var abcder = ABCD.newAbcder(),
+        weekType = abcder.fromWeek(3);
+    expect(weekType).to.eql("C");
+  });
+
+  it("tells fourth week of the year is type D", function () {
+    var abcder = ABCD.newAbcder(),
+        weekType = abcder.fromWeek(4);
+    expect(weekType).to.eql("D");
+  });
+
+  it("tells fifth week of the year is type A again", function () {
+    var abcder = ABCD.newAbcder(),
+        weekType = abcder.fromWeek(5);
+    expect(weekType).to.eql("A");
+  });
 });
